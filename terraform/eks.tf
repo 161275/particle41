1705417c95b1b9cb30d75d3d7912323c84766f9a
nish <nishchalgarg123@gmail.com>
2025-12-26 08:19:11 +0530

    added the nodes to cluster

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -26,9 +26,9 @@ module "eks" {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
-      min_size     = 0
+      min_size     = 2
       max_size     = 4
-      desired_size = 0
+      desired_size = 2
       subnet_ids = [aws_subnet.subnet3.id, aws_subnet.subnet4.id]
     }
   }
